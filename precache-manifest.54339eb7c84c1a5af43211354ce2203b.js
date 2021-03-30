3a2164b4f463a8a92a7bbbf0bf30b7506781dfbb
Yakraj Pariyar <princeyakraj289@gmail.com>
2021-03-30 19:32:08 +0530

    Updates

---
+++ precache-manifest.54339eb7c84c1a5af43211354ce2203b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f078a3c5f0ec69d93c6856c9d675f2",
+    "url": "/prime-video/index.html"
+  },
+  {
+    "revision": "35a60a1acbf5ae51dbc9",
+    "url": "/prime-video/static/css/main.f050090b.chunk.css"
+  },
+  {
+    "revision": "05bd2b10ce20afee2b0b",
+    "url": "/prime-video/static/js/2.e9c979ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prime-video/static/js/2.e9c979ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35a60a1acbf5ae51dbc9",
+    "url": "/prime-video/static/js/main.06cdf66f.chunk.js"
+  },
+  {
+    "revision": "c8fdec451bff03c5e472",
+    "url": "/prime-video/static/js/runtime-main.79d8d145.js"
+  }
+]);
